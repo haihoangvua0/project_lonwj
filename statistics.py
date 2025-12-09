91503ea63b6ede382717ee2fb6f7b8576e03d93a
haihoangvua0 <167439221+haihoangvua0@users.noreply.github.com>
2025-12-09 21:51:50 +0700

    Enhance median function and add fast cumulative function
    Refactor median function to support frequency and add fast function for cumulative sums.

--- statistics.py
+++ statistics.py
@@ -1,21 +1,50 @@
 from process_front_end import *
-def median(l: list[int]):
-        l = sorted(l)
-        n = len(l)
-        if n % 2 == 0:
-                k = n // 2
-                res = (1/2) * (l[k - 1] + l[k])
-                res = int(res) if res.is_integer() else res
-                return res
+
+choice = dict_of_setting["Statistics"]
+def fast(l: list[int]):
+        fast = []
+        for i in range(len(l)):
+                if i == 0:
+                        fast.append(l[0])
+                        continue
+                fast.append(fast[i-1] + l[i])
+        return fast
+
+def median(l: list[int], freq: list | None = None):
+        global choice
+        if freq:
+                if not choice:
+                        #freq = None
+                        l = sorted(l)
+                        n = len(l)
+                        if n % 2 == 0:
+                                k = n // 2
+                                res = (1/2) * (l[k - 1] + l[k])
+                                res = returning(res)
+                                return res
+                        else:
+                                k = (n - 1) // 2
+                                return returning(l[k])
+                        
+                import bisect
+                presum_freq = fast(freq)
+                pass
+
         else:
-                k = (n - 1) // 2
-                return (l[k])
+                if choice:
+                        freq = [1 for _ in l]
+                        presum_freq = fast(freq)
+                        pass
+                
+                
+
 
 # min, max
 
 def tu_phan_vi(l: list[int]):
         l = sorted(l)
         n = len(l)
+        print(l)
         q2 = median(l)
         if n % 2 == 0: 
                 new = l[:(n // 2) + 1] + [q2]
@@ -64,4 +93,5 @@ def do_lech_chuan(l: list[int], freq: list[int] = []):
         return pow(phuong_sai(l, freq), 0.5)
 
 if __name__ == "__main__":
-        print()
+        a = [5.73, 7.05, 6.77, 1.82, 5.64, 6.42]
+        print(tu_phan_vi(a))
