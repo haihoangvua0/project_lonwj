cd7acab6e0f8c0af602bb70573f8735c92ed8ce2
haihoangvua0 <167439221+haihoangvua0@users.noreply.github.com>
2025-12-26 10:32:34 +0700

    Refactor calculator modes and input handling
    Refactor calculator modes and input handling in draft.py.

--- draft.py
+++ draft.py
@@ -2,21 +2,22 @@ import tkinter as tk
 from process_front_end import *
 # Trong process_front_end có các hàm như inte,... và có 3 hàm đặc biệt evaluate_expression, solve_eq, calc
 
-SHIFT_MODE = False
-ALPHA_MODE = False
-CALC_MODE = False
-SOLVE_MODE = False
-#ITER_MODE = False
 class Calculator_fx:  
         #global SHIFT_MODE, ALPHA_MODE, CALC_MODE, SOLVE_MODE
         def __init__(self):  
                 self.win = tk.Tk()  
                 self.win.title("Casio FX Hybrid Simulator")  
                 self.win.geometry("400x600")  
-                self.win.update_idletasks()  
+                self.win.update_idletasks()
+                self.shift = False
+                self.alpha = False
+                self.calc_mode = False
+                self.solve_mode = False
+                self.regulation = "S"
+                self.temp_value = 0
                 #print("Window width:", self.win.winfo_width())  
                 #print("Window height:", self.win.winfo_height())  
-                self.win.attributes("-topmost", True)
+                #self.win.attributes("-topmost", True)
                 self.build_display()
                 self.build_top_keys()
                 self.build_replay()
@@ -44,17 +45,20 @@ class Calculator_fx:
                 """Cho 4 nút điều hướng"""
                 pass
         def on_press3(self, value):
-                global SHIFT_MODE, ALPHA_MODE, CALC_MODE, SOLVE_MODE
                 """Cho bảng số hàm (frac,...)"""
+                self.inputs.focus_set()
                 pos = self.inputs.index(tk.INSERT)
                 if value in ([",", "_", "x", "^-1", "^", "^2", "^3", "(", ")"] + names):
                         self.inputs.insert(pos, value)
                 elif value == "j":
                         if complex_choice:
                                 self.inputs.insert(pos, "*1j")
+                                self.inputs.icursor(pos + 3)
                 elif value in ["inte", "frac", "sqrt", "log", "ln", "sin", "cos", "tan",
                                "d_dx", "sums", "muls"]:
-                        self.inputs.insert(pos, value+"(")
+                        text = value+"("
+                        self.inputs.insert(pos, text)
+                        self.inputs.icursor(pos + len(text))
                 elif value == "OPTN":
                         pass
                 elif value == "CALC":
@@ -64,23 +68,36 @@ class Calculator_fx:
                                 if i in expr:
                                         free_symbol.append(i)
                         if free_symbol:
-                                CALC_MODE = True
-                                dict_of_res = {}
+                                self.calc_mode = True
+                                self.calc_expr = expr
+                                self.calc_vars = free_symbol
+                                self.calc_values = {}
+                                self.calc_index = 0
+
                                 self.inputs.delete(0, tk.END)
                                 self.output.delete(0, tk.END)
-                                for i in free_symbol:
-                                        self.inputs.insert(f"{i}=")
-                                        # Đến đây thì muốn cho số vào output, xong sau khi bấm dấu bằng rồi cho vào dict có sẵn rồi gán nguyên hàm calc vào...
-                                        self.inputs.delete(0, tk.END)
-                                self.inputs.delete(0, tk.END)
-                                self.output.delete(0, tk.END)
-                                self.inputs.insert(0, expr)
-                                self.output.insert(0, f"{calc(expr=expr, **dict_of_res)}")
+                                self.inputs.insert(0, f"{free_symbol[0]}=")
                         else:
-                                self.output.delete(0, tk.END)
-                                self.output.insert(0, f"{evaluate_expression(expr)}")
+                                try:
+                                        res = evaluate_expression(expr)
+                                        self.output.delete(0, tk.END)
+                                        self.output.insert(0, str(res))
+                                except:
+                                        self.output.delete(0, tk.END)
+                                        self.output.insert(0, MATH_ERROR)
+                elif value == "S<=>D":
+                        expr = self.inputs.get()
+                        res = evaluate_expression(expr)
+                        self.temp_value = res
+                        self.output.delete(0, tk.END)
+                        if self.regulation == "S":
+                                self.output.insert(0, returning(res, "D"))
+                                self.regulation = "D"
+                        else:
+                                self.output.insert(0, self.temp_value)
+                                self.regulation = "S"
         def on_press4(self, value):
-                global SHIFT_MODE, ALPHA_MODE, CALC_MODE, SOLVE_MODE
+                #global SHIFT_MODE, ALPHA_MODE, CALC_MODE, SOLVE_MODE
                 """Cho bảng số"""
                 if value == "AC":
                         self.inputs.delete(0, tk.END)
@@ -91,16 +108,47 @@ class Calculator_fx:
                         if pos > 0:
                                 self.inputs.delete(pos - 1, pos)
                                 self.inputs.icursor(pos - 1)
+                        elif pos == 0:
+                                if len(self.inputs.get()) == 0:
+                                        pass # Do nothing
+                                else:
+                                        self.inputs.delete(pos, pos + 1)
                         self.output.delete(0, tk.END)
                 elif value == "=":
-                        try:
-                                expr = self.inputs.get()
-                                result = evaluate_expression(expr)
-                                self.output.delete(0, tk.END)
-                                self.output.insert(0, str(result))
-                        except:
-                                self.output.delete(0, tk.END)
-                                self.output.insert(0, MATH_ERROR)
+                        if not self.calc_mode:
+                                try:
+                                        expr = self.inputs.get()
+                                        result = evaluate_expression(expr)
+                                        self.output.delete(0, tk.END)
+                                        self.output.insert(0, str(result))
+                                except:
+                                        self.output.delete(0, tk.END)
+                                        self.output.insert(0, MATH_ERROR)
+                        else:
+                                try:
+                                        current_var = self.calc_vars[self.calc_index]
+                                        val = evaluate_expression(self.inputs.get().split("=")[1])
+                                        self.calc_values[current_var] = val
+
+                                        self.calc_index += 1
+
+                                        # Còn biến tiếp
+                                        if self.calc_index < len(self.calc_vars):
+                                                self.inputs.delete(0, tk.END)
+                                                self.inputs.insert(0, f"{self.calc_vars[self.calc_index]}=")
+                                        else:
+                                        # Tính xong
+                                                res = calc(expr=self.calc_expr, **self.calc_values)
+                                                self.output.delete(0, tk.END)
+                                                self.output.insert(0, str(res))
+
+                                                self.inputs.delete(0, tk.END)
+                                                self.inputs.insert(0, self.calc_expr)
+
+                                                self.calc_mode = False
+                                except:
+                                        self.output.delete(0, tk.END)
+                                        self.output.insert(0, MATH_ERROR)
                 else:
                         self.output.delete(0, tk.END)
                         # ép Entry lấy lại focus# ép Entry lấy lại focus
@@ -117,36 +165,33 @@ class Calculator_fx:
         def build_top_keys(self):  
                 frame = tk.Frame(self.win)  
                 frame.pack(pady=4)  
-  
-                #self.win.update_idletasks()  
-  
-                for i, txt in enumerate(["SHIFT", "ALPHA", "MODE", "OFF"]):  
-                        tk.Button(  
-                                frame, text=txt,  
-                                width=4, height=1  
-                        ).grid(row=0, column=i, padx=3)  
-  
+                for i, txt in enumerate(["SHIFT", "ALPHA", "MODE", "OFF"]):
+                        tk.Button(
+                                frame, text=txt,
+                                width=4, height=1
+                        ).grid(row=0, column=i, padx=3)
         def build_extra_grid(self):  
-                frame = tk.Frame(self.win)  
-                frame.pack(pady=4)  
+                frame = tk.Frame(self.win)
+                frame.pack(pady=4)
   
                 grid = [  
                         ["OPTN", "CALC", "", "", "inte", "x"],  
                         ["frac", "sqrt", "^2", "^", "log", "ln"],  
                         ["_", "degs", "^-1", "sin", "cos", "tan"],  
                         ["Stor", "j", "(", ")", "S<=>D", "M+"]  
-                ]  
-  
-                #self.win.update_idletasks()  
-  
+                ]
                 for r in range(len(grid)):  
                         for c in range(len(grid[0])):
                                 txt = grid[r][c]
-                                tk.Button(  
-                                        frame, text=txt,  
-                                        width=2, height=2,  
-                                        state="disabled" if txt == "" else "normal" 
-                                ).grid(row=r, column=c, padx=2, pady=2)  
+                                btn = tk.Button(
+                                        frame,
+                                        text=txt,
+                                        width=2, height=2,
+                                        state="disabled" if txt == "" else "normal",
+                                        takefocus=0
+                                )
+                                btn.grid(row=r, column=c, padx=2, pady=2)
+                                btn.bind("<Button-1>", lambda e, t=txt: self.on_press3(t))
   
         def build_replay(self):  
                 frame = tk.Frame(self.win)  
